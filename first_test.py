1c4e6b00ac9ca2fa711688e4ce47282bc4e37f61
Julia <y.mironova@mail.ru>
2020-12-09 13:22:47 +1000

    first commit

---
+++ first_test.py
@@ -0,0 +1,18 @@
+from app.calculator import Calculator
+import pytest
+
+class TestCalc:
+    def setup(self):
+        self.calc=Calculator
+
+    def test_multiply_calculate_corrctly(self):
+        assert self.calc.multiply(self,2,2)==4
+
+    def test_division_calculate_corrctly(self):
+        assert self.calc.division(self,2,2)==1
+
+    def test_subtraction_calculate_corrctly(self):
+        assert self.calc.subtraction(self,2,2)==0
+
+    def test_adding_calculate_corrctly(self):
+        assert self.calc.adding(self,2,2)==4
